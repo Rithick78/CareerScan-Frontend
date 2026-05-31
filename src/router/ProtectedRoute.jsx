0ddd9727a9cdd26dcec65cb5b3e7756755b8a0eb
rithickjackson8 <rithickjackson8@gmail.com>
2026-06-01 00:13:35 +0530

    git commit -m "feat: initialize react application with redux provider"

--- src/router/ProtectedRoute.jsx
+++ src/router/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom'
 
 function ProtectedRoute({ children }) {
   const token = useSelector((state) => state.auth.token)
-  if (!token) return <Navigate to="/login" replace />
+  if (!token) return <Navigate to="/" replace />
   return children
 }
 
